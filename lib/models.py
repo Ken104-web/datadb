35368dcb6faadc69da9544dbd72fb6f3c1a16355
Ken <kennethwarui35@gmail.com>
2024-10-11 23:10:08 +0300

    created employer model

--- lib/models.py
+++ lib/models.py
@@ -1,9 +1,20 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from app import db
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
+
+Class Employer(db.Model):
+    __tablename__ = 'employers'
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+
+    #one to many with employees
+    employees = relationship('Employee', backref='employer')
+
+    
 
 db = SQLAlchemy(metadata=metadata)
 
