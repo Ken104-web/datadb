ef754f68451c87ddb37116087d6e25ac97ebcfd7
Ken <kennethwarui35@gmail.com>
2024-10-14 00:34:36 +0300

    models

--- lib/models.py
+++ lib/models.py
@@ -1,3 +1,4 @@
+from re import T
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from app import db
@@ -6,15 +7,39 @@ metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
+db = SQLAlchemy(metadata=metadata)
+
+
+
+
 Class Employer(db.Model):
     __tablename__ = 'employers'
     id = Column(Integer(), primary_key=True)
     name = Column(String())
 
     #one to many with employees
-    employees = relationship('Employee', backref='employer')
+    employees = relationship('Employee', backref=backref('employer'))
 
-    
+    #one to many with products
+    products = relationship('Product', backref=backref('employer_too'))
 
-db = SQLAlchemy(metadata=metadata)
+
+
+Class Employee(db.Model):
+    __tablename__ = 'employees'
+    id = Column(Interger(), primary_key=True)
+    name = Column(String())
+    salary = Column(Interger())
+
+    # one to many with products
+    products = relationship('Product', backref=backref('employee_name'))
+
+Class Products(db.Model):
+    __tablename__ = 'products'
+    id = Column(Interger(), primary_key=True)
+    product_name = (String())
+    product_price = (Interger())
+    employer_id = Column(Integer(), ForeignKey('employers.id'))
+    employee_id = Column(Integer(), ForeignKey('employees.id'))
+
 
